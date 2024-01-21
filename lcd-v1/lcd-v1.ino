0c9e4b449fa456069f104933f894715d79bad49c
Adrian Taylor <taylor.adrian@gmail.com>
2024-01-21 14:20:52 +0000

    added multiple LCD panel option

--- lcd-v1/lcd-v1.ino
+++ lcd-v1/lcd-v1.ino
@@ -1,9 +1,9 @@
 /**
-    Simple Hauptwerk friendly LCD screen message printer - 
+    Simple Hauptwerk friendly LCD screen message printer -
     receives SysEx from Hauptwerk and dislays on the LCD.
 
     Adrian Taylor, 2024
-    
+
     Developed from:
         SysEx-Read-MAX7219-Message.ino
         Lowell Bahner 2020-02-15
@@ -13,73 +13,87 @@
 
 #include <LiquidCrystal_I2C.h>
 #include <Control_Surface.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+LiquidCrystal_I2C lcd0(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+LiquidCrystal_I2C lcd1(0x21, 16, 2); // I2C address 0x27, 16 column and 2 rows
+
+LiquidCrystal_I2C lcd[2] = {lcd0,lcd1};
 
 USBMIDI_Interface midi;
 
 // function to display the SysEx message on LCD display
 void update_lcd(uint8_t msg[]) {
+  // If this isn't from Hauptwerk, ignore
+  if (msg[1] != 0x7d) return; 
+  // send to LCD 0 by default
+  int dest = 0;
+  // But if we get asked to send to the 2nd LCD, do so
+  if (msg[3] == 0x1)
+    dest = 1;
 
-  lcd.clear();
-  // the sysex message starts with "f0 77 XX ...", start byte, manufacturer code, device code
-  if (msg[1] != 0x7d) return; // check for desired manufacturer code
-  
-//  if (msg[2] == 0x10) lcd.print("SysEx from Device 10:"); // device code 10
-//  if (msg[2] == 0x09) Serial.println("SysEx from Device 09:"); // device code 09
+  // Grab the text string
+  char line[32]; 
+  for (int i = 0; i <= 32; i++) {
+    line[i] = int8_t(msg[i + 6]);
+  }
 
-    char line[32]; // array for the number as char array  
-    for (int i = 0; i <= 32; i++){
-      line[i] = int8_t(msg[i + 5]);
-    }
-    
+  // Setup the screen
+  lcd[dest].clear();
+  lcd[dest].home();
+  lcd[dest].setCursor(0, 0);
   
-    lcd.clear();
-    lcd.home();
-    lcd.setCursor(0,0);
-    for (int i = 0; i <= 16; i++)
+  // Output Line 1 
+  for (int i = 0; i <= 15; i++)
+  {
+    // Skip over non-ascii chars
+    if (isprint(line[i]))
     {
-      if (isprint(line[i]))
-      {
-        lcd.print(line[i]);
-      }
-      else
-      {
-        lcd.print(" ");
-      }
+      lcd[dest].print(line[i]);
     }
-    lcd.setCursor(0,1);
-    for (int i = 17; i <= 32; i++)
+    else
     {
-      if (isprint(line[i]))
-      {
-        lcd.print(line[i]);
-      }
-      else
-      {
-        lcd.print(" ");
-      }   
+      lcd[dest].print(" ");
     }
+  }
+  
+  // Output Line 2
+  lcd[dest].setCursor(0, 1);
+  for (int i = 16; i <= 32; i++)
+  {
+    if (isprint(line[i]))
+    {
+      lcd[dest].print(line[i]);
+    }
+    else
+    {
+      lcd[dest].print(" ");
+    }
+  }
 }
 
 // function to capture the incoming SysEx message
 bool sysExMessageCallback(SysExMessage se) {
-  update_lcd(se.data); // pass the data to MAX7219 function
+  update_lcd(se.data); 
   return true; // Return true to indicate that handling is done,
 }
 
 
 void setup() {
-//  Serial.begin(31250);
-  lcd.init(); // initialize the lcd
-  lcd.backlight();
-  lcd.clear();                 // clear display
-  lcd.setCursor(0, 0);         // move cursor to   (0, 0)
-  lcd.print("Error: Hauptwerk");        // print message at (0, 0)
-  lcd.setCursor(0, 1);         // move cursor to   (2, 1)
-  lcd.print("not running?"); // print message at (2, 1)
+  //  Serial.begin(31250);
+  
+  // Initialise Screens
+  for (int i = 0; i < 2; i++)
+  {
+    lcd[i].init(); // initialize the lcd
+    lcd[i].backlight();
+//    lcd[i].setBacklight(1);
+    lcd[i].clear();                 // clear display
+    lcd[i].setCursor(0, 0);         // move cursor to   (0, 0)
+    lcd[i].print("Screen: ");
+    lcd[i].print(i);
+  }
 
   Control_Surface.begin();
-  Control_Surface.setMIDIInputCallbacks(nullptr, sysExMessageCallback, nullptr, nullptr); 
+  Control_Surface.setMIDIInputCallbacks(nullptr, sysExMessageCallback, nullptr, nullptr);
 }
 
 void loop() {
